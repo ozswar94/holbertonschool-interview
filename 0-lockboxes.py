2ee9fc63ef0783567fdb0fd267b8b674b224a2bc
ozswar94 <jordan6heat@hotmail.fr>
2022-12-07 10:37:41 +0100

    first correction

---
+++ 0-lockboxes.py
@@ -0,0 +1,22 @@
+##!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    ptr1 = 0
+    ptr2 = len(boxes) - 1
+    size = len(boxes) - 1
+
+    while ptr1 < ptr2:
+        if (len(boxes[ptr1]) == 0 and ptr1 != size):
+            return False
+        for i in boxes[ptr1]:
+            if (i > size):
+                return False
+        if (len(boxes[ptr2]) == 0 and ptr2 != size):
+            return False
+        for i in boxes[ptr2]:
+            if i > size:
+                return False
+        ptr1 += 1
+        ptr2 -= 1
+    return True
