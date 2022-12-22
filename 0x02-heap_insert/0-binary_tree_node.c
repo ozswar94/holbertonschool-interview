dbbc390520089b191fb126b8f1c61161e88b0fd9
ozswar94 <jordan6heat@hotmail.fr>
2022-12-22 13:48:39 +0100

    add comment

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - create node on tree
+ * @parent: parent of new node
+ * @value: value of new node
+ *
+ * Return: new node or NULL if failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
