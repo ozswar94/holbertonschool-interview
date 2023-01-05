795066e962c627d0870f9a93f381196fa9062b3d
ozswar94 <jordan6heat@hotmail.fr>
2023-01-05 14:37:11 +0100

    test

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,10 +7,12 @@ in this file: Copy All and Paste
 
 
 def minOperations(n):
+    if not n or not isinstance(n, int) or n <= 1:
+        return 0
     ch = 'H'
     op = 0
     cp = ''
-    while len(ch) < n:
+    while len(ch) != n:
         if len(ch) % 2 or len(ch) == n // 2:
             cp = ch
             ch += cp
