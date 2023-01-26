ba37f2782f163199469ee41a4fa987d2fcf42c04
ozswar94 <jordan6heat@hotmail.fr>
2023-01-26 21:59:59 +0100

    finish

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,6 +9,7 @@ code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
 def getline():
+    """ line """
     print("File size: {}".format(total))
     for c in sorted(code.keys()):
         if code[c] > 0:
