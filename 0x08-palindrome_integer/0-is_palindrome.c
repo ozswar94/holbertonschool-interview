70606b8f8b84b9c392e27f54b9f44ba9257836e4
ozswar94 <jordan6heat@hotmail.fr>
2023-02-09 18:45:30 +0100

    finish

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,32 @@
+/**
+ ** getbeg - finds number number needed to index first digit of n
+ ** @n: number to check
+ **
+ ** Return: beginning index
+ **/
+unsigned long getbeg(unsigned long n)
+{
+	unsigned long beg = 1;
+
+	do
+		beg *= 10;
+
+	while (n /= 10);
+	return (beg / 10);
+}
+
+/**
+ ** is_palindrome - checks palindrome
+ ** @n: number to check
+ **
+ ** Return: 1 if palindrome, otherwise 0
+ **/
+int is_palindrome(unsigned long n)
+{
+	unsigned long beg, end;
+
+	for (beg = getbeg(n), end = 1; beg > end; beg /= 10, end *= 10)
+		if (n / beg % 10 != n / end % 10)
+			return (0);
+	return (1);
+}
