5f06f51963d50ce9c56336afc8ac90ae4fadd633
ozswar94 <jordan6heat@hotmail.fr>
2023-03-30 19:19:12 +0200

    done

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,58 @@
+#include "binary_trees.h"
+
+/**
+ * new_node - create binary tree node
+ * @n: value of node
+ * @par: pointer to parent node
+ *
+ * Return: newly created node
+ */
+avl_t *new_node(int n, avl_t *par)
+{
+	avl_t *new = malloc(sizeof(*new));
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->parent = par;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
+
+/**
+ * binary_traversal - traverse array in binary search pattern and create nodes
+ * @par: pointer to parent node
+ * @a: array of node values
+ * @l: left index
+ * @r: right index
+ *
+ * Return: final created node, NULL on malloc failure
+ */
+avl_t *binary_traversal(avl_t *par, int *a, int l, int r)
+{
+	avl_t *new;
+	int m;
+
+	if (l > r)
+		return (NULL);
+	m = (l + r) / 2;
+	new = new_node(a[m], par);
+	if (!new)
+		return (NULL);
+	new->left = binary_traversal(new, a, l, m - 1);
+	new->right = binary_traversal(new, a, m + 1, r);
+	return (new);
+}
+
+/**
+ * sorted_array_to_avl - build AVL tree from array
+ * @array: pointer to first element of array
+ * @size: size of array
+ *
+ * Return: pointer to root node of created tree, NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	return (binary_traversal(NULL, array, 0, size - 1));
+}
---
+++ 0x0D-sorted_array_to_avl/README.md
@@ -0,0 +1,1 @@
+# 0x0D Sorted Array To Avl
